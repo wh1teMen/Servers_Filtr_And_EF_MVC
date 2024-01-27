6713034341f0ce793c69ac9a433e8e7a817abc34
wh1teMen <aleexandr777@gmail.com>
2024-01-27 22:16:02 +0500

    добавление фильтра

--- MyprogectHW/Controllers/HomeController.cs
+++ MyprogectHW/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace MyprogectHW.Controllers
             db_ = context_;        
         }
        
-        public IActionResult Index(string title)
+        public IActionResult Index(string title,string sort="asc",string change="comp")
         {
             if(title != null)
             {
@@ -33,6 +33,38 @@ namespace MyprogectHW.Controllers
                 var servers = db_.Servers.ToList();
                 var component = db_.Components.ToList();
                 var os = db_.Operations.ToList();
+
+                switch (sort)
+                    {
+                        case "asc": servers = servers.OrderBy(x => x.TitleCompany).ToList(); break;
+                        case "desc": servers = servers.OrderByDescending(x => x.TitleCompany).ToList(); break;
+                        default: break;
+                    }
+
+                switch (change)
+                {
+                    case "comp": if (sort == "asc")
+                        {servers = servers.OrderBy(x => x.TitleCompany).ToList();}
+                        if (sort == "desc") { servers = servers.OrderByDescending(x => x.TitleCompany).ToList();}
+                        break;
+                    case "model":
+                        if (sort == "asc")
+                        { component = component.OrderBy(x => x.TitleModel).ToList();}
+                        if (sort == "desc") { component = component.OrderByDescending(x => x.TitleModel).ToList(); }
+                        break;
+                    case "term":
+                        if (sort == "asc")
+                        { servers = servers.OrderBy(x => x.Expiration_date).ToList(); }
+                        if (sort == "desc") { servers = servers.OrderByDescending(x => x.Expiration_date).ToList(); }
+                        break;
+                    case "osy":
+                        if (sort == "asc")
+                        { os = os.OrderBy(x => x.TitleOS).ToList(); }
+                        if (sort == "desc") { os = os.OrderByDescending(x => x.TitleOS).ToList(); }
+                        break;
+                }
+                
+               
                 CollectionElement collection = new CollectionElement { servers = servers, components = component, operations = os };
                 return View(collection);
 
